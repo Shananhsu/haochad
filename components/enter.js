6bfe721b94d7a6635f8ab5cef51af642cdc8d779
shananhsu <shananhsu@gmail.com>
2022-09-15 16:12:01 +0800

    demo

---
+++ components/enter.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+//style
+import styles from "../styles/enter.module.scss"
+
+const Enter = () => {
+    const [enter, setEnter] = useState(true);
+    const [text, setText] = useState(false);
+    const [video, setVideo] = useState(false);
+
+    const handleEnter = () => {
+
+        setEnter(false);
+        if (enter) {
+            setText(true);
+        }
+        setTimeout(() => {
+            setText(false);
+            setVideo(true);
+
+        }, 9000)
+    }
+
+    return (
+        <>
+            {enter && <div className={styles.enterClick}>
+                <button onClick={handleEnter}>Open</button>
+            </div>
+            }
+            {text && <div className={styles.content}>
+                <p className={styles.enterText}></p>
+            </div>}
+            {video && <div className={styles.contentVideo}>
+                <iframe
+                    src="https://player.vimeo.com/video/237456970?h=7c114d4a43&autoplay=1&loop=1"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            </div>}
+            {video && <div className={styles.contentTop}></div>}
+        </>
+    );
+}
+
+export default Enter;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Enter from "../components/enter";
+
+//styles 
+import styles from "../styles/Home.module.css";
+
+//components
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Enter />
+    </>
+  );
+}
+
+export default Home;
